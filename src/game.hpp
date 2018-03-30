a0884fc2b4dc99fa394eed04bcec2c9575cce2b4
Rajat Silver Khola <rajatsilver03@gmail.com>
2018-03-30 20:22:52 +0530

    initial

---
+++ src/game.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <SFML/Graphices.hpp>
+
+namespace pkmn
+{
+	 class game
+	 {
+	 	 public:
+	 	 static sf:RenderWindow window;
+	 	 
+	 	 private:
+	 	   void init();
+	 	   void process_input();
+	 	   void play_audio();
+	 	   void render();
+	 	   void update();
+	 	   void clean();
+	 }
+}
+
+
---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <game.h>
+
+int main()
+{
+	 
+}
