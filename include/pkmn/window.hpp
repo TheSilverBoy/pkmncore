b51c206473508cf8c4f7f7f390898428e18c9357
Rajat Silver Khola <rajatsilver03@gmail.com>
2018-04-02 20:04:30 +0530

    added window interface

--- include/pkmn/window.hpp
+++ include/pkmn/window.hpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 
 #include <vector>
+#include <array>
 
 namespace pkmn
 {
@@ -12,53 +13,75 @@ namespace pkmn
 	 {
 	 	  unsigned int id;
 	 };
-	
-	 struct window_data
-	 {
-	 	  const char title[20];
-	 	  int x,y;
-	 	  unsigned int w,h;
-	 	  float opacity;
-	 };
 	 
-	 struct window_creation_data
+	 struct window_title_data
 	 {
-	 	  window_data wdata;
-	 	  int fullscreen;
-	 	  unsigned int wflags;
-	 };
-	 
-	 struct window_opacity_data
-	 {
-	 	  float opacity;
+	 	 char title[30];
 	 };
 	 
 	 struct window_dimension_data
 	 {
 	 	  unsigned int w,h;
+	 };
+	 
+	 struct window_position_data
+	 {
+	 	 int x,y;
 	 }
 	 
-	 class window_manager 
+	  struct window_creation_data
+	  {
+	 	  char title[20];
+	 	  int x,y;
+	 	  unsigned int w,h;
+	 	  int fullscreen;
+	 	  unsigned int wflags;
+	  };
+	 
+	 class window_storage
 	 {
-	 	    std::vector<window_data>  m_wdata;
-	 	        std::vector<window_opacity_data>
-	 	    m_wodata;
-	 	  
-	 	    std::vector<window_id> m_wids;
+	 	    std::vector<window_position_data>  m_wpdata;
+	 	   std::vector<float>
+	 	    m_wodata; 
+	 	    std::vector<window_title_data>
+	 	    m_wtdata;
+	 	     std::vector<window_dimension_data> m_wddata;
+	 	 
+	 	    std::vector<bool> m_wtrackers;
 	 	    
 	 	    int count;
 	 	    int size;
+	 	    
+	 	  private:
+	 	    static window_storage* ws_instance;
+	 	  private:
+	 	    window_storage();
+	 	    ~window_storage();
+	 	    
+	 	    void update();
 	 	  public:
+	 	    static window_storage& get_instance();
+	 	  
 	 	    window_id* create_windows(window_creation_data* wcdata, unsigned int count);
 	 	 
 	 	    void destroy_windows(window_id* id, unsigned int count);
 	 	    
-	 	    void set_opacity(window_id* wids, window_opacity_data* opacities, unsigned int count);
+	 	    void set_title(const window_id* wids, window_title_data* wtdata, unsigned int count);
 	 	    
-	 	    void set_dimensions()
+	 	    void set_opacity(const window_id* wids, window_opacity_data* opacities, unsigned int count);
+	 	    
+	 	    void set_position(const window_id* wids, window_position_data* positions, unsigned int count);
+	 	    
+	 	    void set_dimensions(const window_id* wids, window_position_data* wpdata, unsigned int count);
+	 	    
+	 	    const char* get_title(const window_id& id);
+	 	    
+	 	    float get_opacity(const window_id& id);
+	 	    
+	 	    window_position_data get_position(const window_id& id);
+	 	    
+	 	    window_dimension_data get_dimensions(const window_id& id);
 	 };
-	 
-	 
 }
 
 #endif // PKMN_WINDOW_H
